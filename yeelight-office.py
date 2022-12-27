c799e03d279744e97ea4111ef75d6be5747c9f65
Bruno Navert <bruno.navert@gmail.com>
2022-12-27 12:29:09 -0500

    Modify utility Python script to reuse TCP connection

--- yeelight-office.py
+++ yeelight-office.py
@@ -18,7 +18,8 @@ from time import sleep
 from collections import OrderedDict
 
 current_command_id = 0
-debug = False
+bulb_ip = "10.0.0.93"
+port = 55443
 
 def next_cmd_id():
   global current_command_id
@@ -26,26 +27,18 @@ def next_cmd_id():
   return current_command_id
     
 def operate_on_bulb(method: str, params: list):
-  bulb_ip="10.0.0.93"
-  port=55443
   msg = {
     'id': next_cmd_id(),
     'method': method,
     'params': params
   }
-  try:
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if debug: print("connect ",bulb_ip, port ,"...")
-    tcp_socket.connect((bulb_ip, port))
-    command = json.dumps(msg) + "\r\n"
-    if debug: print("sending: ", command)
-    tcp_socket.send(command.encode())
-    resp = tcp_socket.recv(1024)
-    if debug: print("response = ", resp)
-    tcp_socket.close()
-  except Exception as e:
-    print("Unexpected error:", e)
-    traceback.print_exc()
+  command = json.dumps(msg) + "\r\n"
+  if debug: print("sending: ", command)
+  tcp_socket.send(command.encode())
+  resp = json.loads(tcp_socket.recv(1024))
+  if debug: print("response = ", resp)
+  if 'error' in resp:
+    raise RuntimeError('error with operation ' + method, resp['error'].get('message'))       
 
 def set_power(power, effect, duration):
   mode = 2 # RGB
@@ -54,34 +47,39 @@ def set_power(power, effect, duration):
 def set_bright(bright):
   operate_on_bulb("set_bright", [bright])
 
-def set_color(red, green, blue):
-  rgb = (red * 65536) + (green * 256) + blue
-  operate_on_bulb("set_rgb", [rgb])
+def set_color(rgb_hex, effect, duration):
+  rgb = int(rgb_hex, 16)
+  operate_on_bulb("set_rgb", [rgb, effect, duration])
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--debug', action='store_true')
-parser.add_argument('--mode', required=True,
-                    choices=['dnd', 'off', 'work', 'warning'])
-args = parser.parse_args()
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--debug', action='store_true')
+  parser.add_argument('--mode', required=True, type=str,
+                      choices=['dnd', 'off', 'work', 'warning'])
+  args = parser.parse_args()
 
-debug = args.debug
+  debug = args.debug
+  if debug: print('Connect:', bulb_ip, port, '...')
+  tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  tcp_socket.connect((bulb_ip, port))
 
-if args.mode == 'off':
-  set_power('off', 'smooth', 0)
+  if args.mode == 'off':
+    set_power('off', 'smooth', 0)
 
-elif args.mode == 'dnd':
-  set_power('on', 'smooth', 5)
-  set_bright(75)
-  set_color(139, 0, 0)
+  elif args.mode == 'dnd':
+    set_power('on', 'smooth', 5)
+    set_bright(75)
+    set_color('FF0000', 'smooth', 5)
 
-elif args.mode == 'work':
-  set_power('on', 'smooth', 5)
-  set_bright(50)
-  set_color(0, 255, 0)
+  elif args.mode == 'work':
+    set_power('on', 'smooth', 5)
+    set_bright(50)
+    set_color('00FF7F', 'smooth', 5)
 
-elif args.mode == 'warning':
-  set_power('on', 'smooth', 5)
-  set_bright(75)
-  set_color(255, 102, 0)
+  elif args.mode == 'warning':
+    set_power('on', 'smooth', 5)
+    set_bright(75)
+    set_color('FFA500', 'smooth', 5)
   
+  tcp_socket.close()
